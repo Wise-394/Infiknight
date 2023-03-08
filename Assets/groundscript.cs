ff6505c787e23d15702d74c303aa8a0d826e3cf4
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2023-03-08 15:56:36 +0800

    added jump sound effect and improved ui

--- Assets/groundscript.cs
+++ Assets/groundscript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class groundscript : MonoBehaviour
 {
@@ -12,13 +13,14 @@ public class groundscript : MonoBehaviour
     public Animator animator;
     public LogicScript logic;
     public bool isalive = true;
+    public AudioSource jumpsound;
     // Start is called before the first frame update
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("logic").GetComponent<LogicScript>();
         myrigidbody = transform.parent.GetComponent<Rigidbody2D>();
         animator = transform.parent.GetComponent<Animator>();
- 
+        jumpsound = GameObject.FindGameObjectWithTag("sound2").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -28,10 +30,12 @@ public class groundscript : MonoBehaviour
         {
            myrigidbody.velocity = Vector2.up * jumpHeight;
             animator.SetBool("jumping", true);
+            jumpsound.Play();
             onGround = false;
         }
         else if (Input.GetMouseButtonDown(1) && doublejump > 0 && isalive)
         {
+            jumpsound.Play();
             animator.SetTrigger("djump");
             myrigidbody.velocity = Vector2.up * doubleJumpHeight;
             animator.SetBool("jumping", true);
@@ -57,12 +61,14 @@ public class groundscript : MonoBehaviour
     {
         if (onGround && isalive)
         {
+            jumpsound.Play();
             myrigidbody.velocity = Vector2.up * jumpHeight;
             animator.SetBool("jumping", true);
             onGround = false;
         }
         else if (doublejump > 0 && isalive)
         {
+            jumpsound.Play();
             animator.SetTrigger("djump");
             myrigidbody.velocity = Vector2.up * doubleJumpHeight;
             animator.SetBool("jumping", true);
